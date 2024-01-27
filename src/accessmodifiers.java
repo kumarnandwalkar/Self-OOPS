5f2d962fd6c6249c40f659a43eb967839ddb8c71
Kumar Nandwalkar <nandwalkarkumar1@gmail.com>
2024-01-28 01:28:35 +0530

    accessmodifiers and polymorphism added

---
+++ src/accessmodifiers.java
@@ -0,0 +1,25 @@
+public class accessmodifiers
+{
+//    public
+//    public can be accessed from anywhere, inside the class, outside the class, inside package outside package, inside a
+//    subclass, outside a subclass everywhere
+
+//    private
+//    private can only be accessed inside the same class, not even any other class of the same package can access it, nor even
+//    extended subclasses of the same package access it
+
+//    default
+//    this can be accessed only inside the same package
+//    when we do not have anything before the datatype of variable (i.e is public private) it is treated as default
+
+//    protected
+//    protected can not be used outside the package but can only be used if it is beign called in a subclass outside the package,
+//    and it is only accessable in the same package
+
+
+//    pro tip:
+//    we should always keep our instance variable as private , classes as public (note:there can only be one public class inside
+//    a file) should avoid using default access modifier, should prefer using protected for methodes so that we can inherit our
+//    class outside the package and can access the methode specifically
+//
+}
---
+++ src/polymorphism.java
@@ -0,0 +1,13 @@
+public class polymorphism
+{
+//    poly = many,
+//    morphism = behaviour
+//    the one having different behaviour in different situation
+//
+//    2 TYPES OF POLYMORPHSIM
+//    1. compiletime polymorphism
+//    2. runtime polymorphism
+//
+//    overloading is an compiletime polymorphism and overriding is runtime polymorphism
+
+}
