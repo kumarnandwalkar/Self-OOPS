255246edb42300971d319f277d8f7133a8653fa7
Kumar Nandwalkar <nandwalkarkumar1@gmail.com>
2024-02-29 01:55:09 +0530

    sealed class

--- src/sealedclass.java
+++ src/sealedclass.java
@@ -4,8 +4,12 @@
 /*
 sealed class is used when we want only some classes to extend this class, except that class no other class should be able to
 extend this class but also in whichever class it is extended that extended class should be final, non-sealed or seald class
+we can define which class should be able to extend the sealed class and that can be done with permits keyword,
+other than the class mentioned in permits are not allowed to extend the sealed class
 
 we can extend non-sealed class also without any restrictions
+also when we extend this sealed class into any another interface then that interface cannot be final, so when we are extending
+the sealed class that sealed class should be sealed or non-sealed theres no option for final
 
  */
 
@@ -25,6 +29,17 @@ non-sealed class newclassA extends newclass
 }
 
 class newclassB extends newclassA
+{
+
+}
+
+
+sealed interface newinterfaceclass permits newextendedimplementation
+{
+
+}
+
+non-sealed class newextendedimplementation implements newinterfaceclass
 {
 
 }
